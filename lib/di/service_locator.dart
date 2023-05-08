1ed7d66c1fc3de31bd1900d8125706101fdd5f4c
rizkyagungramadhan <rizkyagungramadhan@gmail.com>
2023-05-08 23:54:43 +0700

    feat: setup dependency injection

---
+++ lib/di/service_locator.dart
@@ -0,0 +1,8 @@
+import 'package:get_it/get_it.dart';
+
+GetIt serviceLocator = GetIt.instance;
+
+/// Inject all dependencies
+void setupServiceLocator() {
+  // TODO(agung): Add some service or repository to be injected
+}
