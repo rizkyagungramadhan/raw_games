71b8d0a1e58ec995dc2920ff7c7bf1c447a3d602
rizkyagungramadhan <rizkyagungramadhan@gmail.com>
2023-05-09 02:44:19 +0700

    chore: register routes

--- lib/utils/router/route.dart
+++ lib/utils/router/route.dart
@@ -1,3 +1,5 @@
+import 'package:raw_games/screens/home/home_screen.dart';
+import 'package:raw_games/utils/router/app_router.dart';
 import 'package:raw_games/utils/router/route_definition.dart';
 
 class Routes {
@@ -6,4 +8,13 @@ class Routes {
   static const home = RouteDefinition(name: '/');
 }
 
-// TODO(agung): Create registration for each route here
+class AppRoute {
+  const AppRoute._();
+
+  static void initializeAllRoutes(AppRouter appRouter) {
+    appRouter.registerRoute(
+      Routes.home,
+      (_, __) => const HomeScreen(),
+    );
+  }
+}
