10aa16cda25519ed4bf9d9ab641a96fe5e3a97d9
rizkyagungramadhan <rizkyagungramadhan@gmail.com>
2023-05-14 13:46:57 +0700

    feat: add starter -bash script- pack

---
+++ script/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Running flutter pub get ..."
+if ! flutter pub get ; then
+  echo "ERROR: Failed to run 'flutter pub get'"
+  exit 1
+fi
+
+echo "Running flutter pub run build_runner build --delete-conflicting-outputs ..."
+if ! flutter pub run build_runner build --delete-conflicting-outputs ; then
+  echo "ERROR: Failed to run 'flutter pub run build_runner build --delete-conflicting-outputs'"
+  exit 1
+fi
+
+echo "Done initializing libraries & generated files on this project!"
